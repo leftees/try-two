9aae7d61575c875b3e9ea0b12d51ef93bdb436f2
Vassiliy Pimkin <vassiliy.pimkin@gmail.com>
2016-03-30 22:03:51 +0300

    Found every little file

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,14 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     jasmine: {
-      src: '../js/Application.js',
+      src: 'js/Application.js',
       options: {
-        specs: 'spec/ApplicationSpec.js'
+        specs: 'jasmine/spec/ApplicationSpec.js',
+        vendor: [ 'js/jquery-1.12.2.min.js',
+                  'js/jquery.loadTemplate-1.5.6.min.js',
+                  'jasmine/lib/jasmine-jquery.js' ],
+        helpers: [ 'jasmine/spec/requests.js',
+                   'jasmine/spec/fixtures.js' ]
       }
     }
   });
